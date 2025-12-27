f23eb3a79664b244d1505d20ab8d428dfbac40b5
Mihailo <mihailoless@gmail.com>
2025-12-27 13:39:51 +0100

    Finished the footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,38 @@
+export function Footer() {
+
+  const socialMedia = [
+    {
+      brand: 'fa-facebook',
+      lable: "Visit our Facebook profile",
+      link: '#'
+    },
+    {
+      brand: 'fa-x-twitter',
+      lable: "Visit our X profile",
+      link: '#'
+    },
+    {
+      brand: 'fa-instagram',
+      lable: "Visit our Instagram profile",
+      link: '#'
+    }
+  ]
+
+  return (
+    <footer className="flex flex-col items-center gap-5 text-black">
+      <div className="flex gap-5">
+        {socialMedia.map((icon) => (
+          <a 
+            href={icon.link}
+            aria-label={icon.lable}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className={`fa-brands ${icon.brand} text-(--blue-500) text-2xl px-2 py-2 border rounded-3xl border-(--gray-400) hover:bg() hover:text-white hover:bg-(--blue-500) hover:border-(--blue-500) transition-colors duration-150 ease-in cursor-pointer`}></i>
+          </a>
+        ))}
+      </div>
+      <p className="text-(--gray-400)">© Copyright Ping. All rights reserved.</p>
+    </footer>
+  )
+}
